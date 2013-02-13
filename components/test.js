7f2776c73936693e36e0b8e7c76201cdbe22ef10
Brian Cray <bcrayzie@gmail.com>
2013-02-13 13:02:07 -0800

    Renamed modules to components

---
+++ components/test.js
@@ -0,0 +1,18 @@
+Pilot.Component.define('test', function () {
+"use strict";
+
+return {
+    load: function () {
+        var module = this;
+        this.display();
+        window.setTimeout(function () {
+            module.events.trigger('foo');
+            module.data.set('foo', 'bar');
+        }, 1000);
+    },
+    display: function () {
+        this.element.innerHTML = '<b>' + this.data.get('foo') + '</b>';
+    }
+};
+
+});
---
+++ components/test2.js
@@ -0,0 +1,16 @@
+Pilot.Component.define('test2', function () {
+"use strict";
+
+return {
+    load: function () {
+        var module = this;
+        module.events.on('foo', function () {
+            module.display();
+        });
+    },
+    display: function () {
+        this.element.innerHTML = '<b>test2</b>';
+    }
+};
+
+});
