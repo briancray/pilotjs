cb33b0b3f0ec4358262e48167d3f46e8f6d270f8
Brian Cray <bcrayzie@gmail.com>
2013-02-13 17:22:45 -0800

    Stuff

--- pilot.js
+++ pilot.js
@@ -1,7 +1,42 @@
 ;(function (window, undefined) {
 "use strict";
 
-var Pilot = {};
+var Pilot = {
+    inject: function (file) {
+        var p = Pilot.Promise(),
+            scripts = Pilot.inject.scripts || {};
+        if (scripts[file]) {
+            if (scripts[file] === 1) {
+                return p;
+            }
+            else {
+                p.success();
+                return p;
+            }
+        }
+        var script = document.createElement('script'),
+            when_ready = function () {
+                scripts[file] = 2;
+                p.success();
+            };
+        scripts[file] = 1;
+        script.type = 'text/javascript';
+        if (script.readyState) {
+            script.onreadystatechange = function () {
+                if (script.readyState === 'loaded' || script.readyState === 'complete') {
+                    script.onreadystatechange = null;
+                    when_ready();
+                }
+            };
+        }
+        else { // Others
+            script.onload = when_ready;
+        }
+        script.src = file;
+        document.head.appendChild(script);
+        return p;
+    }
+};
 
 window.Pilot = Pilot;
 
