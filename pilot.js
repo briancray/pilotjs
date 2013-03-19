2491cef9d32ab9f7dddf53a36309a239de7a866c
Brian Cray <briancray@Brians-MacBook-Pro.local>
2013-03-19 09:40:07 -0400

    Updates to the newest version of tinyamd

--- pilot.js
+++ pilot.js
@@ -1,150 +1,215 @@
-(function (global, undefined) {
+;(function (global, undefined) {
 "use strict";
 
-var obj = {};
-var obj_tostring = obj.toString;
 var doc = document;
 var el_head = doc.head || doc.getElementsByTagName("head")[0] || doc.documentElement;
-var module_scripts = el_head.getElementsByClassName('pilot-module');
-var pilot_node = (function (scripts) {
+var node = (function (scripts) {
     return scripts[scripts.length - 1];
 })(doc.getElementsByTagName('script'));
-var amd_path = pilot_node.src.slice(0, pilot_node.src.lastIndexOf('/') + 1);
-
-var pilot = {};
-var exports = pilot.exports = {};
-var module = pilot.module = {};
-
-var define = pilot.define = function (id, dependencies, factory) {
-    var id_path_split, id_path, id_name, ready, module_script, arg_count = arguments.length;
-
-    if (typeof id === 'string' && exports[id].status === 2) {
-        return;
-    }
-    else if (arg_count <= 2) {
-        module_script = module_scripts[module_scripts.length - 1].src;
-        module_script = module_script.slice(amd_path.length, -3);
-        if (arg_count === 1) {
-            factory = id;
-            id = module_script;
-            dependencies = ['require', 'exports', 'module'];
-        }
-        else {
-            if (obj_tostring.call(id) === '[object Array]') {
-                factory = dependencies;
-                dependencies = id;
-                id = module_script;
+var main = node.getAttribute('data-main');
+var anonymous_queue = [];
+var settings = {
+    baseUrl: (function (href) {
+        var place = href.split('/').slice(0, 3).join('/');
+        var path;
+        if (main) {
+            if (main.slice(0, place.length) === place) {
+                path = main;
+            }
+            else if (main[0] === '/') {
+                path = place + main;
             }
             else {
-                factory = dependencies;
-                dependencies = ['require', 'exports', 'module'];
+                path = href.slice(0, href.lastIndexOf('/') + 1) + main;
             }
+            main = main.slice(main.lastIndexOf('/') + 1);
+        }
+        else {
+            path = href;
+        }
+        return path.slice(0, path.lastIndexOf('/') + 1);
+    })(global.location.href.split('?')[0])
+};
+var exports = {};
+
+function config (config) {
+    extend(settings, config);
+};
+
+function define (id, dependencies, factory) {
+    var arg_count = arguments.length;
+
+    if (arg_count === 1) {
+        factory = id;
+        dependencies = ['require', 'exports', 'module'];
+        id = null;
+    }
+    else if (arg_count === 2) {
+        if (pilot.toString.call(id) === '[object Array]') {
+            factory = dependencies;
+            dependencies = id;
+            id = null;
+        }
+        else {
+            factory = dependencies;
+            dependencies = ['require', 'exports', 'module'];
         }
     }
+    else if (!dependencies.length) {
+        dependencies = ['require', 'exports', 'module'];
+    }
 
-    ready = function () {
+    if (!id) {
+        anonymous_queue.push([dependencies, factory]);
+        return;
+    }
+
+    function ready () {
         var handlers = exports[id].handlers;
-        var module = exports[id] = typeof factory === 'function' ? factory.apply(null, arguments) : factory;
-        module.status = 2;
-        handlers && handlers.forEach(function (handler) {
-            handler && handler(module);
-        });
+        var context = exports[id].context;
+        var module = exports[id] = typeof factory === 'function' ? factory.apply(null, anonymous_queue.slice.call(arguments, 0)) || exports[id] : factory;
+        module.pilot = 2;
+        module.context = context;
+        for (var x = 0, xl = handlers.length; x < xl; x++) {
+            handlers[x](module);
+        }
     };
-    id_path_split = id.lastIndexOf('/'),
-    id_path = (id_path_split !== -1 ? id.slice(0, id_path_split) : '').split('/'),
-    id_name = id.slice(id_path_split + 1);
 
-    dependencies = dependencies.map(function (dependency, i) {
-        var absolute_path = ''; 
-        if (dependency.indexOf('/') === -1) {
-            absolute_path = dependency;
-        }   
-        else if (dependency.indexOf('..') === 0) {
-            absolute_path = id_path.slice(0, -1).join('/') + dependency.slice(2);
-        }   
-        else if (dependency.indexOf('.') === 0) {
-            absolute_path = id_path.join('/') + dependency.slice(1);
-        }   
-        return absolute_path || dependency;
-    }); 
-
-    require(dependencies, ready);
+    require(dependencies, ready, id);
 };
 
 define.amd = {};
 
-var require = pilot.require = function (module, callback) {
-    var loaded, loaded_modules, script;
+function require (module, callback, context) {
+    var loaded_modules = [];
 
-    if (obj_tostring.call(module) === '[object Array]') {
-        loaded = 0;
-        loaded_modules = [];
-        module.filter(function (m) {
-            return m && typeof m === 'string';
-        }).forEach(function (m, i) {
-            m && typeof m === 'string' && require(m, function (def) {
-                loaded_modules[i] = def;
-                ++loaded === module.length && callback.apply(null, loaded_modules);
-            });
-        });
-        return;
-    }
-    else if (!module || typeof module !== 'string') {
-        return;
-    }
-    else {
-        module = module.toLowerCase();
-        if (module === 'require' || module === 'exports' || module === 'module') {
-            callback(global[module]);
-            return;
+    if (pilot.toString.call(module) === '[object Array]') {
+        for (var x = 0, xl = module.length; x < xl; x++) {
+            switch (module[x]) {
+                case 'require':
+                    var _require = function (new_module, callback) {
+                        return require(new_module, callback, context);
+                    };
+                    _require.toUrl = toUrl;
+                    loaded_modules[x] = _require;
+                    break;
+                case 'exports':
+                    loaded_modules[x] = exports[context];
+                    break;
+                case 'module':
+                    loaded_modules[x] = {
+                        id: context,
+                        uri: toUrl(context)
+                    };
+                    break;
+                case exports[context] ? exports[context].context : '':
+                    loaded_modules[x] = exports[exports[context].context];
+                    break;
+                default:
+                    (function (x) {
+                        require(module[x], function (def) {
+                            loaded_modules[x] = def;
+                            loaded_modules.length === module.length && callback && callback.apply(null, loaded_modules);
+                        }, context);
+                    })(x);
+            };
         }
+        if (loaded_modules.length === xl && callback) {
+            callback.apply(null, loaded_modules);
+        }
+        return;
     }
+
+    module = context ? toUrl(module, context) : module;
      
     if (exports[module]) {
-        switch (exports[module].status) {
-            case 1:
-                callback && exports[module].handlers.push(callback);
-                break;
-            case 2:
-                callback && callback(exports[module]);
+        if (exports[module].pilot === 1) {
+            callback && exports[module].handlers.push(callback);
         }
+        else {
+            callback && callback(exports[module]);
+        }
+        return exports[module];
     }
     else {
         exports[module] = {
-            status: 1,
-            handlers: [callback]
+            pilot: 1,
+            handlers: [callback],
+            context: context
         };
     }
+    
+    inject(toUrl(module), function () {
+        var queue_item;
+        if (queue_item = anonymous_queue.shift()) {
+            queue_item.unshift(module);
+            exports[module].pilot === 1 && define.apply(null, queue_item);
+        }
+    });
+};
 
-    script = doc.createElement('script');
+var toUrl = require.toUrl = function (id, context) {
+    var new_context, i;
+    switch (id) {
+        case 'require':
+        case 'exports':
+        case 'module':
+            return id;
+    }
+    new_context = (context || settings.baseUrl).split('/');
+    new_context.pop();
+    id = id.split('/');
+    i = id.length;
+    while (--i) {
+        switch (id[0]) {
+            case '..':
+                new_context.pop();
+                id.shift();
+                break;
+            case '.':
+            case '':
+                id.shift();
+        }
+    }
+    return (new_context.length ? new_context.join('/') + '/' : '') + id.join('/') + (context === undefined && !/\.[a-z]{1,3}$/.test(id[id.length - 1]) ? '.js' : '');
+};
+
+function inject (file, callback) {
+    var script = doc.createElement('script');
     script.onload = script.onreadystatechange = function () {
         if (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete') {
             script.onload = script.onreadystatechange = null;
             el_head.removeChild(script);
+            callback && callback();
         }
     };
     script.type = 'text/javascript';
     script.async = true;
-    script.className = 'pilot-module';
-    script.src = amd_path + module + '.js';
+    script.src = file;
     el_head.appendChild(script);
 };
 
-var extend = pilot.extend = function (target, source) {
+function extend (target, source) {
     for (var x in source) {
         source.hasOwnProperty(x) && (target[x] = source[x]);
     }
     return target;
 };
 
-var get_type = pilot.get_type = function (v) {
-    return obj_tostr.call(o).slice(8, -1).toLowerCase();
+function get_type (v) {
+    return pilot.toString.call(v).slice(8, -1);
 };
 
-global.define = define;
-global.require = require;
-global.pilot = pilot;
+global.pilot = {
+    config: config,
+    inject: inject,
+    define: global.define = define,
+    require: global.require = require,
+    exports: exports,
+    extend: extend,
+    get_type: get_type
+};
 
-require(pilot_node.getAttribute('data-main'));
+main && require(main);
 
 })(this);
